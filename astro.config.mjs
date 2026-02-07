d9fe00147f1fd73d246a5f86b45b174245fdae04
alanskull09 <92652658+alanskull09@users.noreply.github.com>
2026-02-06 23:58:38 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-// https://astro.build/config
-export default defineConfig({});
-=======
 export default defineConfig({
-  site: 'https://alanskull09.github.io',
-  base: '/AlanSkull/',
+  site: "https://alanskull09.github.io",
+  base: "/AlanSkull",
 });
->>>>>>> 67c98db (add github pages workflow)
-=======
-export default defineConfig({
-  site: 'https://alanskull.com',
-  base: '/',
-});
->>>>>>> 3972432d9adbcf5053c5183c963cf551d0ab69c6
