778fcd97077f47212fbef9cec0547fa1429b0680
Alan Oliver López Robles <alanskull09@gmail.com>
2026-02-07 00:10:43 -0600

    fix: resolve conflicts and restore astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://alanskull09.github.io",
   base: "/AlanSkull",
 });
+=======
+	import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://alanskull.com",
+});
+
+>>>>>>> e684be2 (fix: resolve conflicts and restore astro build)
